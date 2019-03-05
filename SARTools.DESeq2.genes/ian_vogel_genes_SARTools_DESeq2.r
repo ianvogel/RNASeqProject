9708c0c51d9f84b09fd5115e6c175e095f66b52b
ianvogel <irv001@bucknell.edu>
2019-03-05 16:07:36 -0500

    class batch stuff

--- SARTools.DESeq2.genes/ian_vogel_genes_SARTools_DESeq2.r
+++ SARTools.DESeq2.genes/ian_vogel_genes_SARTools_DESeq2.r
@@ -10,12 +10,12 @@
 ################################################################################
 rm(list=ls())                                        # remove all the objects from the R session
 
-workDir <- "~/Documents/Bio 364/RNAseq Project 3/SARTools.DESeq2.genes"      # working directory for the R session
+workDir <- "~/Documents/Bio 364/RNAseq Project 3/SARTools.DESeq2.genes_batch"      # working directory for the R session
 
-projectName <- "SARTools.DESeq2.genes"                         # name of the project
+projectName <- "SARTools.DESeq2.transcripts"                         # name of the project
 author <- "Ian Vogel"                                # author of the statistical analysis/report
 
-targetFile <- "../SARtools/genes.target.txt"                           # path to the design/target file
+targetFile <- "../SARtools/transcripts.target.txt"                           # path to the design/target file
 rawDir <- "../SARtools"                                      # path to the directory containing raw counts files
 featuresToRemove <- NULL    # names of the features to be removed
                         # (specific HTSeq-count information and rRNA for example)
@@ -23,7 +23,7 @@ featuresToRemove <- NULL    # names of the features to be removed
 
 varInt <- "Treatment"                                    # factor of interest
 condRef <- "Untreated"                                      # reference biological condition
-batch <- NULL                                        # blocking factor: NULL (default) or "batch" for example
+batch <- "batch"                                       # blocking factor: NULL (default) or "batch" for example
 
 idColumn = 1                                         # column with feature Ids (usually 1)
 countColumn = 5                                      # column with counts  (2 for htseq-count, 7 for featurecounts, 5 for RSEM/Salmon, 4 for kallisto)
